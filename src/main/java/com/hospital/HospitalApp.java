6cb67fc616d8782f2d2f3f200e215c1ae2bacfc0
Bubblefdm <Bubblefdm@163.com>
2019-09-25 12:10:24 +0800

    111

--- src/main/java/com/hospital/HospitalApp.java
+++ src/main/java/com/hospital/HospitalApp.java
@@ -3,10 +3,17 @@ package com.hospital;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.hospital.dao")
-public class HospitalApp {
+public class HospitalApp  extends SpringBootServletInitializer {
+        //    使用自带tomcat启动
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HospitalApp.class);
+    }
     public static void main(String[] args){
         SpringApplication.run(HospitalApp.class,args);
     }
